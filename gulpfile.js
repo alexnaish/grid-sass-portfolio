50f19d5d0ea3f4e72454a85d58e97708a2b47f52
Alex Naish <lexi.drage@gmail.com>
2015-01-03 22:56:29 +0000

    removing sourcemap files and adding gulp + package.json

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    del = require('del');
+
+
+
+gulp.task('sass', function() {
+    return sass('sass/style.scss', {style: 'expanded'})
+        .pipe(autoprefixer(['last 2 versions']))
+        .pipe(gulp.dest('css'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('css'))
+        .pipe(notify({
+            message: 'Styles task complete'
+        }));
+});
+
+// Watch
+gulp.task('watch', function() {
+    // Watch .scss files
+    gulp.watch('sass/*.scss', ['sass']);
+
+});
+
+gulp.task('default', function() {
+    gulp.start('sass');
+});
